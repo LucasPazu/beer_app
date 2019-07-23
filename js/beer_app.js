63acd95888df97a0e1b63b545287d61811365217
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2019-07-24 00:01:10 +0200

    do the first design

--- js/beer_app.js
+++ js/beer_app.js
@@ -1,5 +1,27 @@
 (async () => {
 
+    /* Selectors */
+    const randomBeerBtn = document.querySelector('#random-beer');
+
+
     const rootEndpoint = 'https://api.punkapi.com/v2/';
+
+    function searchRandomBeer() {
+        const url = rootEndpoint + '/beers/random';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+            location.reload();
+    }
+
+    /* Event Listeners */
+    randomBeerBtn.addEventListener('click', searchRandomBeer);
+   
 
 })();
